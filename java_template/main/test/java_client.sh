453f3be183fe58c0c137999d9b87c6afab12a852
david <daperez@uw.edu>
2019-12-11 18:14:13 -0800

    added experimental data + graphs for all services

--- java_template/main/test/java_client.sh
+++ java_template/main/test/java_client.sh
@@ -28,7 +28,7 @@ json={"\"filterBy\"":$filterByAsString,"\"aggregateBy\"":$aggregateByAsString,"\
 
 echo "Invoking Lambda function service 1 load and transform using AWS cli"
 #time output=`curl -s -H "Content-Type: application/json" -X POST -d  $json https://yc67ejcsk9.execute-api.us-east-1.amazonaws.com/ServiceOne_dev`
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name  ServiceOne --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+#time output=`aws lambda invoke --invocation-type RequestResponse --function-name  ServiceOne --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 
 echo ""
 echo "CURL ENCODE RESULT:"
@@ -48,7 +48,10 @@ echo $output
 echo ""
 
 echo "Invoking Lambda function service 3 query and write as json"
-#time output=`aws lambda invoke --invocation-type RequestResponse --function-name ServiceThree --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+
+echo $aggregateByAsString
+echo $filterByAsString
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name ServiceThree --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
 echo ""
 echo "AWS CLI RESULT:"
 echo $output
